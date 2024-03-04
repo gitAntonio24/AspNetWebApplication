1cce7d595105ebff1bda5e9bb158efb136abe252
Harys-Randria <harysrandrianomenjanahary@gmail.com>
2024-03-04 14:26:25 +0300

    add class

---
+++ AspNetWebApplication/models/Utilisateur.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetWebApplication.Models
+{
+    public class Utilisateur
+    {
+		private string _nomUtilisateur;
+		private string _motdepasse;
+		private string _email;
+
+       
+
+        public string Email
+		{
+			get { return _email; }
+			set { _email = value; }
+		}
+
+
+		public string MotDePasse
+		{
+			get { return _motdepasse; }
+			set { _motdepasse = value; }
+		}
+
+
+		public string NomUtilisateur
+		{
+			get { return _nomUtilisateur; }
+			set { _nomUtilisateur = value; }
+		}
+
+        public Utilisateur() { }
+
+        public Utilisateur(string nomUtilisateur, string motdepasse, string email)
+        {
+            _nomUtilisateur = nomUtilisateur;
+            _motdepasse = motdepasse;
+            _email = email;
+        }
+
+    }
+}
